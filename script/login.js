00a8759bd83cf43f065870221264b04aa54e9d5f
unknown <sandrasindhu0701@gmail.com>
2024-02-18 13:54:12 +0530

    todo

--- script/login.js
+++ script/login.js
@@ -1,14 +1,14 @@
- // Login Form Submission
- document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+// Login Form Submission
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-    // Check login credentials
-    if (username === 'admin' && password === '12345') {
-      // Redirect to main page
-      window.location.href = 'home.html';
-    } else {
-      alert('Invalid login credentials');
-    }
-  });
+  // Check login credentials
+  if (username === 'admin' && password === '12345') {
+    // Redirect to the main page
+    window.location.href = 'home.html';
+  } else {
+    alert('Invalid login credentials');
+  }
+});
